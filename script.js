af7bb7e8ee1367ce0b61a1ea754d0543fbf45ff9
Maximilian <scientificresearch309@gmail.com>
2024-11-18 22:55:34 +0100

    It works with a normal function and still not using a IIFE

--- script.js
+++ script.js
@@ -12,7 +12,15 @@ and now explain to YOURSELF (or someone around you) WHY this worked? Take all th
 GOOD LUCK 😀
 */
 
-(function () {
-  const header = document.querySelector("h1");
+// (function () {
+//   const header = document.querySelector("h1");
+//   header.style.color = "red";
+// })();
+
+const header = document.querySelector("h1");
+const body = document.querySelector("body");
+
+body.addEventListener("click", () => {
+  console.log("Hallo");
   header.style.color = "red";
-})();
+});
